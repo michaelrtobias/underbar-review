f9a061eeb45452342ac2b3c63e1b96ebad583a02
Michael Tobias <michaelremitobias@gmail.com>
2020-08-18 19:59:47 -0700

    "Finished Some"

--- src/underbar.js
+++ src/underbar.js
@@ -229,7 +229,9 @@
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
     var iterator = iterator || _.identity;
-
+    return !_.every(collection, function (item) {
+      return !iterator(item);
+    });
   };
 
 
